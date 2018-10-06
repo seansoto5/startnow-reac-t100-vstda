29343e1c3ed039514dbebaec12d9b0df9a4666f9
seansoto5 <sean_soto125@yahoo.com>
2018-10-05 18:29:17 -0700

    Added initial project files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className='container'>
+      <header>
+        <h1>Very Simple To Do App</h1>
+        <h4>Track all of the things</h4>
+      </header>
+      <hr></hr>
+        <div className='row'>
+          <div className='column'>
+
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
